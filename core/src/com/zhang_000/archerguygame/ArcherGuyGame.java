43a717374761f4a9f12b02cbfa0b633607acc41f
KennysFoot <busstopbench@gmail.com>
2015-06-06 14:13:23 -0400

    added splash screen

--- core/src/com/zhang_000/archerguygame/ArcherGuyGame.java
+++ core/src/com/zhang_000/archerguygame/ArcherGuyGame.java
@@ -2,14 +2,14 @@ package com.zhang_000.archerguygame;
 
 import com.badlogic.gdx.Game;
 import com.zhang_000.archerguygame.helper_classes.AssetLoader;
-import com.zhang_000.archerguygame.screens.GameScreen;
+import com.zhang_000.archerguygame.screens.SplashScreen;
 
 public class ArcherGuyGame extends Game {
 
 	@Override
 	public void create () {
 		AssetLoader.load();
-		setScreen(new GameScreen());
+		setScreen(new SplashScreen(this));
 	}
 
 	@Override
