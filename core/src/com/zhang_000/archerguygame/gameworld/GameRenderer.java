8396bd5f57cf85eb4e8a241b46c567add88b8cfd
KennysFoot <busstopbench@gmail.com>
2015-06-06 11:38:56 -0400

    ayy

--- core/src/com/zhang_000/archerguygame/gameworld/GameRenderer.java
+++ core/src/com/zhang_000/archerguygame/gameworld/GameRenderer.java
@@ -4,19 +4,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.zhang_000.archerguygame.helper_classes.AssetLoader;
 
 public class GameRenderer {
 
-    GameWorld world;
+    private GameWorld world;
+    private final float GAME_WIDTH;
+    private final float GAME_HEIGHT;
 
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
 
+    //ASSETS
+    private TextureRegion archerGuy;
+
     public GameRenderer(GameWorld world, float gameWidth, float gameHeight) {
         this.world = world;
+        GAME_WIDTH = gameWidth;
+        GAME_HEIGHT = gameHeight;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(true, gameWidth, gameHeight);
@@ -26,10 +35,15 @@ public class GameRenderer {
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(camera.combined);
 
+        storeAssetReferences();
+    }
+
+    private void storeAssetReferences() {
+        this.archerGuy = AssetLoader.archerGuy;
     }
 
     public void render(float delta, float runTime) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
@@ -37,5 +51,11 @@ public class GameRenderer {
         shapeRenderer.setColor(0, 0, 1, 1);
         shapeRenderer.rect(rect.x, rect.y, rect.width, rect.height);
         shapeRenderer.end();
+
+        batch.begin();
+        batch.draw(archerGuy, (GAME_WIDTH - archerGuy.getRegionWidth()) / 2,
+                (GAME_HEIGHT - archerGuy.getRegionHeight()) / 2,
+                archerGuy.getRegionWidth(), archerGuy.getRegionHeight());
+        batch.end();
     }
 }
--- core/src/com/zhang_000/archerguygame/helper_classes/AssetLoader.java
+++ core/src/com/zhang_000/archerguygame/helper_classes/AssetLoader.java
@@ -1,8 +1,19 @@
 package com.zhang_000.archerguygame.helper_classes;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class AssetLoader {
 
+    public static TextureRegion archerGuy;
+
     public static void load() {
+        Texture archerGuyTex =  new Texture(Gdx.files.internal("archer_guy_front.png"));
+        archerGuyTex.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+        archerGuy = new TextureRegion(archerGuyTex);
+        archerGuy.flip(false, true);
+
 
     }
 
